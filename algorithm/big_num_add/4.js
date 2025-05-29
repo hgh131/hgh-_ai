ca5171ac16108b0210e22d1784055c2ac134b24e
hgh131 <2232163359@qq.com>
2025-05-29 16:55:45 +0800

    腾讯题

---
+++ algorithm/big_num_add/4.js
@@ -0,0 +1,4 @@
+const bigNum=123456789012345678901234567890123456789n;
+const theNum=BigInt(123456789012345678901234567890123456789);
+console.log(bigNum,theNum,typeof bigNum,typeof 1);
+console.log(bigNum+1n);
